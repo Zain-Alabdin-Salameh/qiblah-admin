1e9a5a3ad8860302cd3a868f371e7e9c22be1b89
Zain Alabdin Salameh <zain97208@gmail.com>
2023-01-24 13:17:30 +0200

    final updates

--- app.js
+++ app.js
@@ -1,57 +1,72 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+const mysqlStore = require("express-mysql-session")(session);
+var bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api");
+const PORT= process.env.APP_PORT;
+const IN_PROD = process.env.NODE_ENV === 'production'
+const TWO_HOURS = 1000 * 60 * 60 * 2
+const options = {
+  connectionLimit: 10,
+  password: process.env.DB_PASS,
+  user: process.env.DB_USER,
+  database: process.env.MYSQL_DB,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  createDatabaseTable: true,
+};
+const sessionStore = new mysqlStore(options);
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', usersRouter);
-app.use(session({
-  secret: process.env.SESSION_SECRET??"session123",
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: true ,maxAge: 3600000 }
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", usersRouter);
+app.use(
+  session({
+    name: process.env.SESS_NAME,
+    secret: process.env.SESSION_SECRET ?? "session123",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: { maxAge: TWO_HOURS, sameSite: true, secure: IN_PROD },
+  })
+);
 
-app.use('/', indexRouter);
-
-
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,26 @@ const prisma = new Prisma.PrismaClient();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  if (!req.session) {
+  console.log(req.session.userId);
+  if (!req.session.userId) {
     res.redirect("/login");
   } else {
+    console.log(req.session.userId);
     let data = await prisma.user.findMany();
     res.render("index", { data: data, session: req.session });
   }
 });
 router.get("/login", function (req, res, next) {
-  res.render("login", { title: "Express" });
+  res.render("login");
 });
 
 router.post("/login", async function (req, res) {
   console.log(req.body);
   const { email, password } = req.body;
-  console.log(email);console.log(password);
+  console.log(email);
+  console.log(password);
   try {
-    if (email!=null & password!=null) {
+    if ((email != null) & (password != null)) {
       console.log("asddddddd");
       var user = await prisma.admin.findFirst({ where: { email: email } });
       console.log(user);
@@ -30,7 +33,8 @@ router.post("/login", async function (req, res) {
       } else {
         if (user.password === password) {
           console.log(req.session);
-          req.session.user = user;
+          // req.session.save()
+          req.session.userId = user.id;
           res.redirect("/");
         } else {
           res.send("Incorrect password");
@@ -42,9 +46,42 @@ router.post("/login", async function (req, res) {
       res.end();
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.send("Please Enter Email Address and Password Details");
     res.end();
+  }
+});
+
+router.get("/changepassword", async function (req, res, next) {
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    res.render("pass", { session: req.session });
+  }
+});
+
+router.post("/changepassword", async function (req, res, next) {
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    const { userId } = req.session;
+    const { password, newpassword } = req.body;
+    try {
+      let admin = await prisma.admin.findFirst({ where: { id: userId } });
+      if (admin.password == password) {
+        const updateUser = await prisma.admin.update({
+          where: { id: userId },
+          data: {
+            password: newpassword,
+          },
+        });
+        res.redirect("/");
+      } else {
+        res.send("incorrect password");
+      }
+    } catch (error) {
+      res.redirect("/login");
+    }
   }
 });
 module.exports = router;
